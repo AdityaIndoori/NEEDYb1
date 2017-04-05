f82696609f2b1b7925c52e7e19a1dc62886cba48
aditya <indooriaditya@gmail.com>
2017-04-05 14:57:38 +0530

    completed HMart upto payment details...still havent used databases for cart

--- app/src/main/java/com/example/aditya/menuview/H_MartPaymentOptions.java
+++ app/src/main/java/com/example/aditya/menuview/H_MartPaymentOptions.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.text.Html;
 import android.widget.TextView;
 
-public class H_MartPaymentOptions extends AppCompatActivity {
+public class  H_MartPaymentOptions extends AppCompatActivity {
     TextView textViewPaymentDetailsText,textViewPaymentDetailsNumbers;
     private int bill, items, deliveryCharges, ExpressDeliveryCharges,total;
     private String billingText,billingNumber;
@@ -20,18 +20,18 @@ public class H_MartPaymentOptions extends AppCompatActivity {
         bill = getIntent().getIntExtra("bill",0);
         items = getIntent().getIntExtra("items",0);
         deliveryCharges = items * 20;
-        ExpressDeliveryCharges = deliveryCharges * 2;
+        ExpressDeliveryCharges = deliveryCharges/2;
         billingText = "Payable Amount:<br><br>Ordered Items:<br><br>Delivery Charges:<br><br>";
-        billingNumber = "Rs."+bill+"<br><br>"+ ""+items+"<br><br>"+ "Rs"+deliveryCharges+"<br><br>";
+        billingNumber = "Rs."+bill+"<br><br>"+ ""+items+"<br><br>"+ "₹ "+deliveryCharges+"<br><br>";
         if (isExpressDelivery){
             total = bill + deliveryCharges + ExpressDeliveryCharges;
             billingText=billingText.concat("Express Delivery Charges:<br><br><b><big><font color=\"#000000\">Total Bill</font></b>");
-            billingNumber=billingNumber.concat("Rs"+ExpressDeliveryCharges+"<br><br>"+"<b><big><font color=\"#000000\">Rs"+total+"</font></b>");
+            billingNumber=billingNumber.concat("₹ "+ExpressDeliveryCharges+"<br><br>"+"<b><big><font color=\"#000000\">₹ "+total+"</font></b>");
         }
         else{
             total = bill + deliveryCharges;
             billingText=billingText.concat("<b><big><font color=\"#000000\">Total Bill:</font></b>");
-            billingNumber = billingNumber.concat("<b><big><font color=\"#000000\">Rs"+total+"</font></b>");
+            billingNumber = billingNumber.concat("<b><big><font color=\"#000000\">₹ "+total+"</font></b>");
         }
         textViewPaymentDetailsText.setText(Html.fromHtml(billingText));
         textViewPaymentDetailsNumbers.setText(Html.fromHtml(billingNumber));
